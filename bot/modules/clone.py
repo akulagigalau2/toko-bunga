0ba102a3b344e009ef54fbd15df484c02c95676b
akulagigalau2 <97729284+akulagigalau2@users.noreply.github.com>
2022-08-29 18:52:29 +0700

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -9,7 +9,7 @@ from bot.helper.telegram_helper.message_utils import sendDump, sendMessage, dele
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
-from bot import dispatcher, LOGGER, STOP_DUPLICATE, download_dict, download_dict_lock, Interval
+from bot import dispatcher, DUMP_CHAT_ID, LOGGER, STOP_DUPLICATE, download_dict, download_dict_lock, Interval
 from bot.helper.ext_utils.bot_utils import is_gdrive_link, new_thread
 
 
